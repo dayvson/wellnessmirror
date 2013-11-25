54a85bf52a5181da5bd823da45d559cd81f95a3b
Maxwell Da Silva <dayvson@gmail.com>
2013-11-24 22:21:17 -0500

    Using only one strip leds, keep things simples

--- wellnessmirror/wellnessmirror.ino
+++ wellnessmirror/wellnessmirror.ino
@@ -12,19 +12,15 @@ long time;
 int alpha;
 int periode = 2000;
 int totalStrips = TOTAL_STRIPS;
-Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(28, 6, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(74, 5, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strips[2] = {strip1, strip2};
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(102, 5, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   initLeds();
 }
 
 void initLeds(){
-  for(int i = 0; i<totalStrips; i++){
-      strips[i].begin();
-      strips[i].show();
-  }
+  strip.begin();
+  strip.show();
 }
 void loop() {
   fadeSlow(0, 0, 255);
@@ -46,12 +42,10 @@ void fadePixels(int time, int r, int g, int b){
 }
 
 void tintPixels(int r, int g, int b, int a){
- for(int s = 0; s<totalStrips; s++){
-    strips[s].setBrightness(a);
-    for(uint16_t i=0; i<strips[s].numPixels(); i++) {
-        uint32_t c = strips[s].Color(r, g, b);
-        strips[s].setPixelColor(i,c);
-    }
-    strips[s].show();
+  strip.setBrightness(a);
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      uint32_t c = strip.Color(r, g, b);
+      strip.setPixelColor(i,c);
   }
+  strip.show();
 }
