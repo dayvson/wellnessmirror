cd9ce812a8a863fc0f7b972174ad676ad8f12860
Maxwell Da Silva <dayvson@gmail.com>
2013-12-09 01:31:10 -0500

    Updating the patterns test

--- patterns_usertest/patterns_usertest.ino
+++ patterns_usertest/patterns_usertest.ino
@@ -1,69 +1,187 @@
-#include <Adafruit_NeoPixel.h>
+//Created by Maria Paula Saba and Maxwell Dayvson da Silva
 
+#include <Adafruit_NeoPixel.h>
+#include <Bridge.h>
+#include <HttpClient.h>
+#include <math.h>
+
+#define TIMECTL_MAXTICKS  4294967295L
+#define TIMECTL_INIT      0
 long time;
+unsigned long flashTimeMark=0;
+unsigned long flashTimeMark2=0;
+long interval = 2000; 
+long previousMillis = 0;
 int alpha;
-int periode = 1000;
-int s;
+int periode = 2000;
+char *very_active = "5";
+char *sendentary = "20";
+char *fairly_active = "10";
+char *lightly_active = "25";
+char *sleep_time = "40";
+int r = 0;
+int g = 0;
+int b = 255;
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(102, 6, NEO_GRB + NEO_KHZ800);
 
-struct RGB {
-  byte r;
-  byte g;
-  byte b;
-};
-
-
-
 void setup() {
-  // put your setup code here, to run once:
+  initLeds();
+}
+
+void initLeds(){
   strip.begin();
   strip.show();
-  
-
 }
 
+int index = 0;
 void loop() {
-
-//choose color
-//  RGB color = { 255 , 255, 255}; //white
-//  RGB color = { 255 , 0 , 255};  //pink
-  RGB color = { 255 , 0 , 0 };   //red
-//  RGB color = { 255 , 155 , 0 }; //orange
-//  RGB color = { 255 , 255 , 0 }; //yellow
-//  RGB color = { 0 , 255 , 255 }; //turquoise
-//  RGB color = { 0 , 0 , 255 };   //blue  
-
-//choose speed
-//  s = 1; 
-    s = 2; 
-//  s = 3;
-//  s = 5;
-//  s = 10;
-
-  //fading slow
-  //fadeSlow(color.r, color.g, color.b, s);
-   
-  //fading fast 
-  //fadeFast(color.r, color.g, color.b, s);
- 
-  //no fading
- // tintPixels(color.r, color.g, color.b, 255);
-
-  tintPixels2ColorsLeftRight(color.r, color.g, color.b, 0, 255, 255, 255);
+  r = 255;
+  g = 0;
+  b = 0;
+  if(waitTime(&flashTimeMark2, 6000)){
+      index+=1;
+      if(index > 7 ) index = 1;
+  }
+  
+  if(index == 1){ 
+   setColorAndPattern(255,0,255, 2);   
+  }else if(index == 2){
+    setColorAndPattern(255,155,0, 3);
+  }else if(index == 3){
+    setColorAndPattern(0,255,255, 4);
+  }else if(index == 4){
+    setColorAndPattern(0,0,255, 5);
+  }else if(index == 5){
+    setColorAndPattern(255,255,0, 6); 
+  }else if (index == 6) {
+    very_active = "5";
+    sendentary = "20";
+    fairly_active = "10";
+    lightly_active = "25";
+    sleep_time = "40";
+    activities(); 
+  }else if( index == 7 ){
+    very_active = "20";
+    sendentary = "10";
+    fairly_active = "40";
+    lightly_active = "1";
+    sleep_time = "29";
+    activities();
+  }
 }
 
+void setColorAndPattern(int r, int g, int b, int pattern){
+  switch (pattern) {
+    case 1:    
+      off(r, g, b);
+      break;
+    case 2:
+      sedentary(r, g, b);
+      break;
+    case 3:
+      lazy(r, g, b);
+      break;
+    case 4:
+      ideal(r, g, b);
+      break;
+    case 5:
+      vigorous(r, g, b);
+      break;
+    case 6:
+      hyperative(r, g, b);
+      break;
+    default:
+      notrecorded(r, g, b);
+  }  
+}
 
+void activities() {
+  int last = atoi(sleep_time) + 2;
+  strip.setBrightness(100);
+  setPixelColorByRange(0, last, 0,0,255);
+  setPixelColorByRange(last, last+atoi(sendentary), 0,255,255);
+  last+=atoi(sendentary);
+  setPixelColorByRange(last, last+atoi(lightly_active), 255,255,0);
+  last+=atoi(lightly_active);
+  setPixelColorByRange(last, last+atoi(fairly_active), 255,155,0);
+  last+=atoi(fairly_active);
+  setPixelColorByRange(last, last+atoi(very_active), 255,0,0);
 
-void fadeSlow(int r, int g, int b, int freq){
-  time = millis()/freq;
+}
+
+void setPixelColorByRange(int start, int _end, int r, int g, int b){
+  while(start<_end) {
+    if(waitTime(&flashTimeMark, 10)){
+      strip.setPixelColor(start,strip.Color(r, g, b));
+      strip.show();
+      start++;
+    }   
+  }
+}
+
+void off(int r, int g, int b){
+  tintPixels(r, g, b, 0);  
+}
+
+void sedentary(int r, int g, int b){
+  time = millis()/2;
   fadePixels(time, r, g, b);
 }
 
-void fadeFast(int r, int g, int b, int freq){
-  time = millis()*freq;
+void lazy(int r, int g, int b){
+ int dur = 100;
+ int pos = 0;
+ while(pos<dur){
+   if(waitTime(&flashTimeMark, 10)){
+    alpha = easeInOutBounce(pos, 0, 100, 100);
+    tintPixels(r, g, b, alpha);
+    pos++;
+   }
+ }
+ pos = 0;
+ while(pos<dur){
+   if(waitTime(&flashTimeMark, 10)){
+    alpha = linearTween(pos, 100, -100, 100);
+    tintPixels(r, g, b, alpha);
+    pos++;
+   }
+ }
+}
+
+void notrecorded(int r, int g, int b){
+  tintPixels(r, g, b, 100);
+}
+
+void hyperative(int r, int g, int b){
+  time = millis()*10;
   fadePixels(time, r, g, b);
 }
 
+void vigorous(int r, int g, int b){
+  time = millis()*5;
+  fadePixels(time, r, g, b);
+}
+
+void linearFadeAnimation(int r, int g, int b, int _start, int _end, int _dur){
+  int alpha = 0;
+  for (int pos=0; pos<_dur;){
+    if(waitTime(&flashTimeMark, 10)){
+      alpha = linearTween(pos, _start, _end, _dur);
+      tintPixels(r, g, b, alpha);
+      pos++;
+    }
+  }
+}
+void ideal(int r, int g, int b){
+  tintPixels(r,g,b,100);
+  if(waitTime(&flashTimeMark, 3000)){
+    int dur = 20;  
+    for(int i = 0; i<3; i++){
+     linearFadeAnimation(r,b, g, 100, -100, dur);
+     linearFadeAnimation(r,b, g, 0, 100, dur);
+    }
+  }
+}
 
 void fadePixels(int time, int r, int g, int b){
   alpha = 128+127*cos(2*PI/periode*time);
@@ -79,40 +197,51 @@ void tintPixels(int r, int g, int b, int a){
   strip.show();
 }
 
-void tintPixels2ColorsLeftRight(int r1, int g1, int b1, int r2, int g2, int b2, int a){
-  strip.setBrightness(a);
-  for(uint16_t i=0; i<strip.numPixels()/2-12; i++) {
-      uint32_t c = strip.Color(r1, g1, b1);
-      strip.setPixelColor(i,c);
-  }
-  
-    for(uint16_t i=strip.numPixels()/2-12; i<strip.numPixels()-12; i++) {
-      uint32_t c = strip.Color(r2, g2, b2);
-      strip.setPixelColor(i,c);
-  }
-  
-    for(uint16_t i=strip.numPixels()-12; i<strip.numPixels(); i++) {
-      uint32_t c = strip.Color(r1, g1, b1);
-      strip.setPixelColor(i,c);
-  }
-  strip.show();
+
+float linearTween (float t, float b, float c, float d) {
+  return c*t/d + b;
 }
 
-void tintPixels2ColorsUpDown(int r1, int g1, int b1, int r2, int g2, int b2, int a){
-  strip.setBrightness(a);
-  for(uint16_t i=0; i<14; i++) {
-      uint32_t c = strip.Color(r2, g2, b2);
-      strip.setPixelColor(i,c);
+float easeInOutSine (float t, float b, float c, float d) {
+  return -c/2 * (cos(M_PI*t/d) - 1) + b;
+}
+
+float easeInBounce (float t, float b, float c, float d) {
+  return c - easeOutBounce (d-t, 0, c, d) + b;
+}
+
+float easeOutBounce (float t, float b, float c, float d) {
+  if ((t/=d) < (1/2.75)) {
+    return c*(7.5625*t*t) + b;
+  } else if (t < (2/2.75)) {
+    return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+  } else if (t < (2.5/2.75)) {
+    return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+  } else {
+    return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
   }
-  
-    for(uint16_t i=14; i<strip.numPixels()/2+12; i++) {
-      uint32_t c = strip.Color(r1, g1, b1);
-      strip.setPixelColor(i,c);
+}
+
+float easeInOutBounce (float t, float b, float c, float d) {
+  if(t < d/2){
+    return easeInBounce (t*2, 0, c, d) * .5 + b;
   }
-  
-    for(uint16_t i=strip.numPixels()/2+12; i<strip.numPixels(); i++) {
-      uint32_t c = strip.Color(r2, g2, b2);
-      strip.setPixelColor(i,c);
+  return easeOutBounce (t*2-d, 0, c, d) * .5 + c*.5 + b;
+}
+
+int waitTime(unsigned long *timeMark, unsigned long timeInterval){
+  unsigned long timeCurrent;
+  unsigned long timeElapsed;
+  int result = false;
+  timeCurrent = millis();
+  if(timeCurrent<*timeMark){
+    timeElapsed=(TIMECTL_MAXTICKS-*timeMark)+timeCurrent;
+  }else{
+    timeElapsed=timeCurrent-*timeMark;  
   }
-  strip.show();
+  if(timeElapsed>=timeInterval) {
+    *timeMark=timeCurrent;
+    result=true;
+  }
+  return(result);  
 }
