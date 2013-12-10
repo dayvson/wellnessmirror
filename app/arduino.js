a76f4315bd0a9ed1c12451a43d4609a2e003760c
Maxwell Da Silva <dayvson@gmail.com>
2013-12-10 16:51:19 -0500

    Small refactoring to make things pretty

--- app/arduino.js
+++ app/arduino.js
@@ -4,25 +4,25 @@ var ArduinoProxy = {
 };
 
 ArduinoProxy.sendOverall = function(color, pattern){
-  try{
+
   request.get(ArduinoProxy.host + "/data/put/r/" + color[0]);
   request.get(ArduinoProxy.host + "/data/put/g/" + color[1]);
   request.get(ArduinoProxy.host + "/data/put/b/" + color[2]);
   request.get(ArduinoProxy.host + "/data/put/pattern/" + pattern);
   request.get(ArduinoProxy.host + "/data/put/mode/2"); //overall
-  }catch(e){};
+
   return true;
 };
 
 ArduinoProxy.sendActivities = function(activities){
-  try{
+
   request.get(ArduinoProxy.host + "/data/put/sleep_time/" + activities.sleepTime);
   request.get(ArduinoProxy.host + "/data/put/very_active/" + activities.veryActive);
   request.get(ArduinoProxy.host + "/data/put/sendentary/" + activities.sedentary);
   request.get(ArduinoProxy.host + "/data/put/fairly_active/" + activities.fairly);
   request.get(ArduinoProxy.host + "/data/put/lightly_active/" + activities.lightly);
   request.get(ArduinoProxy.host + "/data/put/mode/1"); //stats
-  }catch(e){};
+
 };
 
 
